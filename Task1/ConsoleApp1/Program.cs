f704c62a2de621605d83eb9b90cfd53ade392d4d
alicebeth-m <lizamonka@mail.ru>
2023-09-29 03:29:09 +0300

    Task1 BERT

---
+++ Task1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1;
+
+namespace MyApp 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hobbit = File.ReadAllText(args[0]);
+            string modelFilePath = "bert-large-uncased-whole-word-masking-finetuned-squad.onnx";
+            Task result = Process(modelFilePath, hobbit);
+        }
+
+        static async Task Process(string modelFilePath, string hobbit)
+        {
+            var ct = new CancellationTokenSource();
+            var cancel = ct.Token;
+            var nugetResult = new NuGetBert(modelFilePath, cancel);
+            var tasks = new List<Task>();
+            var download_res = nugetResult.download();
+            
+            while (!cancel.IsCancellationRequested)
+            {
+                Console.Write("Ask me smth: ");
+                string question = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(question))
+                    ct.Cancel();
+                var sentence = $"{{\"question\": {question}, \"context\": \"@CTX\"}}".Replace("@CTX", hobbit);
+                //Console.WriteLine(question);
+                var task = nugetResult.TokenizeText(sentence).ContinueWith(t =>
+                {
+                    Console.WriteLine(t.Result);
+                });
+                tasks.Add(task);
+
+            }
+            await Task.WhenAll(tasks.ToArray());
+            
+            
+        }
+
+    }
+
+
+}
